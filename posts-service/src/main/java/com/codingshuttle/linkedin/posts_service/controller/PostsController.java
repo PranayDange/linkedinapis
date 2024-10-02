a09ca70bc107bfe7cdd6be91b5d7d5b3d216d069
PranayDange <pranaydange04@gmail.com>
2024-10-02 16:25:01 +0530

    configurations and empty connection service

--- posts-service/src/main/java/com/codingshuttle/linkedin/posts_service/controller/PostsController.java
+++ posts-service/src/main/java/com/codingshuttle/linkedin/posts_service/controller/PostsController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/posts")
+@RequestMapping("/core")
 @RequiredArgsConstructor
 @Slf4j
 public class PostsController {
